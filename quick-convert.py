ee4d4809cee9d2ae5e4d9bf895e276ce4655637e
blloop <blloop@uw.edu>
2022-04-27 14:13:38 -0700

    Apply grid formatting

--- quick-convert.py
+++ quick-convert.py
@@ -1,17 +1,18 @@
 import tkinter as tk
+from tkinter import *
 from tkinter.messagebox import showinfo
+
+# Create root window
+win = tk.Tk()
+win.resizable(False, False)
+win.geometry('600x400+200+100')
+win.title('Quick Convert')
 
 # Import images
 temp_icon = tk.PhotoImage(file = './img/temperature.png')
 dist_icon = tk.PhotoImage(file = './img/distance.png')
 mass_icon = tk.PhotoImage(file = './img/mass.png')
 time_icon = tk.PhotoImage(file = './img/time.png')
-
-# Create root window
-win = tk.Tk()
-win.resizable(False, False)
-win.geometry('600x400+200+100')
-win.title('Image Button Demo')
 
 # Clear widgets from window
 def reset(win):
@@ -22,16 +23,16 @@ def reset(win):
 # Create frame header
 def makeHeader(win):    
     title = tk.Label(win, text = 'Select a measurement converter: ')
-    title.place(x = 0, y = 0)
+    title.place(height = 30, x = 0, y = 0)
     leave = tk.Button(win, text = 'Quit', command=win.destroy)
-    leave.place(x = 550, y = 0)
+    leave.place(height = 40, width = 100, x = 500, y = 0)
 
 # Test method for clicking
 def clicked():
     showinfo(title = 'Alert!', message = 'Button Clicked!')
 
 # Create landing page
-def makeLanding(win):
+def makeLanding(win): 
     temp = tk.Button(
         win, image = temp_icon, text = 'TEMPERATURE',
         compound = tk.LEFT, command = clicked
@@ -48,10 +49,10 @@ def makeLanding(win):
         win, image = time_icon, text = 'TIME',
         compound = tk.LEFT, command = clicked
     )
-    temp.pack(ipadx=50,ipady=5,expand=True)
-    dist.pack(ipadx=50,ipady=5,expand=True)
-    mass.pack(ipadx=50,ipady=5,expand=True)
-    time.pack(ipadx=50,ipady=5,expand=True)
+    temp.place(height = 180, width = 300, x = 0, y = 40)
+    dist.place(height = 180, width = 300, x = 300, y = 40)
+    mass.place(height = 180, width = 300, x = 0, y = 220)
+    time.place(height = 180, width = 300, x = 300, y = 220)
 
 makeHeader(win)
 makeLanding(win)
