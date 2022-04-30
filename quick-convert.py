3b26dcbc897163be86da842e2b902778f6709a45
blloop <blloop@uw.edu>
2022-04-29 23:26:38 -0700

    Complete home screen

--- quick-convert.py
+++ quick-convert.py
@@ -18,42 +18,51 @@ time_icon = tk.PhotoImage(file = './img/time.png')
 def reset(win):
     for widget in win.winfo_children():
         widget.destroy()
-    makeHeader(win)
-
-# Create frame header
-def makeHeader(win):    
-    title = tk.Label(win, text = 'Select a measurement converter: ')
-    title.place(height = 30, x = 0, y = 0)
-    leave = tk.Button(win, text = 'Quit', command=win.destroy)
-    leave.place(height = 40, width = 100, x = 500, y = 0)
 
 # Test method for clicking
 def clicked():
     showinfo(title = 'Alert!', message = 'Button Clicked!')
 
 # Create landing page
-def makeLanding(win): 
+def makeLanding(win):
+    
+    # Create header
+    title = tk.Label(
+        win, text = 'Select a measurement converter: ',
+        font = ("Verdana", 14), bg = "#99CEFF"
+    )
+    leave = tk.Button(
+        win, text = 'Quit',command=win.destroy,
+        font = ("Verdana", 14), bg = "#FF9999"
+    )
+    title.place(height = 40, width = 500, x = 0, y = 0)
+    leave.place(height = 40, width = 100, x = 500, y = 0)
+
+    # Create image buttons
     temp = tk.Button(
         win, image = temp_icon, text = 'TEMPERATURE',
-        compound = tk.LEFT, command = clicked
+        compound = tk.LEFT, font = ("Verdana", 14),
+        command = clicked, bg = "white", activebackground = "black"
     )
     dist = tk.Button(
         win, image = dist_icon, text = 'DISTANCE',
-        compound = tk.LEFT, command = clicked
+        compound = tk.LEFT, font = ("Verdana", 14),
+        command = clicked
     )
     mass = tk.Button(
         win, image = mass_icon, text = 'MASS',
-        compound = tk.LEFT, command = clicked
+        compound = tk.LEFT, font = ("Verdana", 14),
+        command = clicked, bg = "white"
     )
     time = tk.Button(
         win, image = time_icon, text = 'TIME',
-        compound = tk.LEFT, command = clicked
+        compound = tk.LEFT, font = ("Verdana", 14),
+        command = clicked, bg = "white"
     )
     temp.place(height = 180, width = 300, x = 0, y = 40)
     dist.place(height = 180, width = 300, x = 300, y = 40)
     mass.place(height = 180, width = 300, x = 0, y = 220)
     time.place(height = 180, width = 300, x = 300, y = 220)
 
-makeHeader(win)
 makeLanding(win)
 win.mainloop()
